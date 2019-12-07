8fa8dbd362ba719b06a3ec05ab11900bf05fc9d2
JungJinHwan <yadot1478@naver.com>
2019-12-07 17:43:56 +0900

    버튼 누르면 토스트메시지 나오게 하기

--- app/src/main/java/jinhwan/com/androidappstudy/MainActivity.java
+++ app/src/main/java/jinhwan/com/androidappstudy/MainActivity.java
@@ -2,14 +2,23 @@ package jinhwan.com.androidappstudy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button myButton;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButton = findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "메세지!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
