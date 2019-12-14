435e7d1b097d3a48335d681730d675c206de6297
JungJinHwan <yadot1478@naver.com>
2019-12-14 18:07:57 +0900

    로그인액티비티 뷰와 연동

--- app/src/main/java/jinhwan/com/androidappstudy/activities/LoginActivity.java
+++ app/src/main/java/jinhwan/com/androidappstudy/activities/LoginActivity.java
@@ -1,11 +1,18 @@
 package jinhwan.com.androidappstudy.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import jinhwan.com.androidappstudy.R;
 import jinhwan.com.androidappstudy.activities.base.BaseActivity;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    private EditText login_edit_id, login_edit_pw;
+    private Button login_button;
+
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_login;
@@ -19,6 +26,19 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
     }
 
+    private void initView() {
+        login_edit_id = findViewById(R.id.login_edit_id);
+        login_edit_pw = findViewById(R.id.login_edit_pw);
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String id = login_edit_id.getText().toString().trim();
+    }
 }
